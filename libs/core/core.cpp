60ed546c87517af854755c94cb1d6408bac8663a
Willu12 <boryskurdek@gmail.com>
2024-11-18 13:59:24 +0100

    added size functionality

--- libs/core/core.cpp
+++ libs/core/core.cpp
@@ -1,10 +1,21 @@
 #include "core.hpp"
 #include <vector>
-
+#include <numeric>
 namespace core
 {
 
 multiGraph::multiGraph(const std::vector<std::vector<std::size_t>>& adjacencyMatrix) {
     this->adjacencyMatrix = adjacencyMatrix;
 }
+
+std::size_t multiGraph::size() const {
+    std::size_t size = 0;
+    for (const auto& row : adjacencyMatrix) {
+        for (const auto& edges : row) {
+            size += edges;
+        }
+    }
+    return size;
+}
+
 } // namespace core
