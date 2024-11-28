d2ef06b105caf4fb87e2903ae6f570201211a996
mytkom <marek.mytkowski.mm@gmail.com>
2024-11-28 22:34:01 +0100

    remove not used code

--- libs/hamilton/include/atsp_solver.hpp
+++ libs/hamilton/include/atsp_solver.hpp
@@ -23,18 +23,6 @@ class ATSPSolver {
     Matrix cost_matrix_;
     std::size_t n_;
 
-    // Helper function to compute the Minimum Spanning Tree (MST)
-    std::vector<std::vector<std::size_t>> computeMST();
-
-    // Helper function to compute an improved matching (compared to Christofides' greedy approach)
-    std::vector<std::pair<std::size_t, std::size_t>> computeImprovedMatching(
-        const std::vector<std::size_t>& odd_vertices);
-
-    // Helper function to convert Eulerian Circuit into Hamiltonian Cycle
-    std::vector<std::size_t> eulerianToHamiltonian(const std::vector<std::size_t>& eulerian_circuit);
-
-    std::size_t tsp(std::size_t pos, std::size_t mask, std::vector<std::vector<std::size_t>>& dp,
-                    std::vector<std::vector<std::size_t>>& parent);
     Matrix reconstruct_cycle(const std::vector<std::size_t>& path);
 };
 
